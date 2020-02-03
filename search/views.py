551fb7027b4a9db90c8dc36458eba66e0d41d71d
utkarshpatel500 <utkarshpatel500@gmail.com>
2020-02-04 01:55:16 +0530

    Updated

--- search/views.py
+++ search/views.py
@@ -2,16 +2,52 @@ import requests
 
 from django.shortcuts import render
 from django.conf import settings
+# from isodate import parse_duration
 
 def index (request):
     search_url = 'https://www.googleapis.com/youtube/v3/search'
+    video_url = 'https://www.googleapis.com/youtube/v3/videos'
 
-    params = {
+    search_params = {
         'part' : 'snippet',
         'q' : 'learn python',
-        'key' : settings.YOUTUBE_DATA_API_KEY
+        'key' : settings.YOUTUBE_DATA_API_KEY,
+        'maxResults' : 9,
+        'type' : 'video'
     }
 
-    r=requests.get(search_url,params=params)
-    print(r.text)
-    return render(request,  'search/index.html')
+    video_ids=[]
+    r=requests.get(search_url,params=search_params)
+
+    results = r.json()['items']
+
+    for result in results:
+        video_ids.append(result['id']['videoId'])
+
+    video_params = {
+        'key' : settings.YOUTUBE_DATA_API_KEY,
+        'part' : 'snippet,contentDetails',
+        'id' : ','.join(video_ids),
+        'maxResults' : 9
+    }
+
+    r=requests.get(video_url,params=video_params)
+
+    results = r.json()['items']
+
+    videos = []
+
+    for result in results:
+        video_data = {
+            'title' : result['snippet']['title'],
+            'id' : result['id'],
+            'duration' : result['contentDetails']['duration'],
+            'thumbnail' : result['snippet']['thumbnails']['high']['url']
+        }
+
+        videos.append(video_data)
+    context = {
+        'videos' : videos
+    }
+
+    return render(request,  'search/index.html',context)
